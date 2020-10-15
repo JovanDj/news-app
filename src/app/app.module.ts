d4a651da93eee0510a5bfa4833b9d0ecdd23b0f1
Jovan <jovandjukic2@gmail.com>
2020-10-15 19:21:02 +0200

    Remove pwa and ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
-import { ServiceWorkerModule } from "@angular/service-worker";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { environment } from "../environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
@@ -13,13 +11,11 @@ import { NavbarComponent } from "./navbar/navbar.component";
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomeComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: "serverApp" }),
+    BrowserModule,
     CoreModule,
     AppRoutingModule,
     NgbModule,
-    ServiceWorkerModule.register("/news-app/ngsw-worker.js", {
-      enabled: environment.production
-    }),
+
     RouterModule
   ],
 
